b37271b77200c42bb2d46bf8ce884fb99de8f22e
Joseph Platta <jwplatta@gmail.com>
2026-09-08 22:10:29 -0500

    update prod compose with dsl jobs

---
+++ jobs/compact_non_spxw_option_samples.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require "tickrake"
+
+Tickrake.job "compact_non_spxw_option_samples" do
+  provider :schwab
+  type :maintenance
+
+  schedule do
+    at "15:40"
+    weekdays
+  end
+
+  maintenance do
+    compact :option_samples,
+            universes: ["stock_option_symbols", "etf_option_symbols"],
+            delete_sources: true
+    archive :option_samples,
+            universes: ["stock_option_symbols", "etf_option_symbols"],
+            to: :s3_archive, artifacts: %i[csv parquet],
+            retain: { parquet: true }
+  end
+end
---
+++ jobs/futures_candles.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require "tickrake"
+
+Tickrake.job "futures_candles" do
+  provider :schwab
+  type :candles
+  symbols "/ES", "/NQ", "/RTY", "/YM"
+  lookback 90.days
+
+  schedule do
+    at "16:30"
+    weekdays
+  end
+
+  candles do
+    frequencies "day", "30min", "5min", "1min"
+    start_date "2026-06-01"
+  end
+end
