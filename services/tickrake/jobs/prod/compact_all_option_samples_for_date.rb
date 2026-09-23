fda11bf90e272ae34807323eff86ce03322c0de4
Joseph Platta <jwplatta@gmail.com>
2026-09-23 10:00:49 -0500

    feat: add job to compact option samples and ad hoc script

--- services/tickrake/jobs/prod/compact_all_option_samples_for_date.rb
+++ services/tickrake/jobs/prod/compact_all_option_samples_for_date.rb
@@ -1,17 +1,30 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+# One-shot DSL job for a single option-sample date.
+#
+# Usage:
+#   ruby compact_all_option_samples_for_date.rb 2026-09-21
+
+require "date"
 require "tickrake"
 
-Tickrake.job "compact_all_option_samples" do
+usage = "Usage: ruby compact_all_option_samples_for_date.rb YYYY-MM-DD"
+abort usage unless ARGV.length == 1
+
+begin
+  sample_date = Date.iso8601(ARGV.fetch(0))
+rescue Date::Error
+  abort usage
+end
+
+Tickrake.job "compact_all_option_samples_for_date" do
   provider :schwab
 
-  schedule do
-    at "15:10"
-    weekdays
-  end
-
   maintenance do
+    start_date sample_date
+    end_date sample_date
+
     compact :option_samples, universe: "spx_symbols", delete_sources: true
     archive :option_samples, universe: "spx_symbols",
             to: :s3_archive, artifacts: %i[csv parquet],
